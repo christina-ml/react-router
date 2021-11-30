9c98b988e9a4ff9cb164bc6d12176c0d3a6055a5
Christina Loiacono <loiaconochristina@gmail.com>
2021-11-30 11:29:34 -0500

    added Navbar and TestComp put into Route, with a Switch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from "react";
-import { Router } from "react-router";
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 import './App.css';
+
+function TestComp(){
+  return (
+    <div>Test Component that's returning JSX</div>
+  )
+}
 
 function App(){
   return(
     <Router>
       <div>
         {/* Navbar */}
+        <Link to="/test">Test Link</Link>
 
         {/* Routes */}
+        <Switch>
+          <Route path="/test">
+            <TestComp />
+          </Route>
+        </Switch>
+
       </div>
     </Router>
   )
